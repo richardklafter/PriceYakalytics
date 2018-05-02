6bcc943ba8403bc68dceba362ba127cb5470be48
Richard Klafter <richard@zinc.com>
2018-05-01 17:30:30 -0700

    first template render

--- server.js
+++ server.js
@@ -12,12 +12,16 @@ const url = require('url');
 const express = require('express')
 const request = require('request-promise-native')
 const cookieParser = require('cookie-parser')
+const expressHandlebars  = require('express-handlebars');
 
 const ZINC_CLIENT_ID = process.env['ZINC_CLIENT_ID'] //Your zinc client id. We pass it in as an env var so we can open source this application
 const ZINC_CLIENT_SECRET = process.env['ZINC_CLIENT_SECRET'] //Your zinc client secret. Super secret treat it like a password
 const COOKIE_SECRET = process.env['COOKIE_SECRET'] || ZINC_CLIENT_SECRET //Used to sign cookies so we know they came from us
 
 const app = express(COOKIE_SECRET)
+app.engine('handlebars', expressHandlebars())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
@@ -29,10 +33,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-    var redirectUrl = `${req.protocol}://${req.get('host')}/oauth` //The url we end up at after oauth
+    var redirect_uri = `${req.protocol}://${req.get('host')}/oauth` //The url we end up at after oauth
     var scopes = ['openid', 'priceyak:listing_template', 'priceyak:stores'] //These are the list of scopes PriceYakalytics needs to get the users stores and modify their templates
     var loginUrl = `https://login.priceyak.com/oidc/auth?response_type=code&scope=${scopes.join(',')}&client_id=${ZINC_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirect_uri)}`
-    //XXX: Render the "Login with PY page"
+    res.render('login', { loginUrl:loginUrl })
 })
 
 app.get('/oauth', (req, response) => {
@@ -43,7 +47,7 @@ app.get('/oauth', (req, response) => {
         return
     }
     //We can use the code query param to get a long lived token which can be used call Zinc's API in the future
-    var redirect_uri = `${req.protocol}://${req.get('host')}/login`
+    var redirect_uri = `${req.protocol}://${req.get('host')}/oauth`
     request('https://login.zinc.io/oidc/token', {
         method: "POST",
         json: {
